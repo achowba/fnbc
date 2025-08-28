5cbf67f5f80c401e668baec0aef4fd7f65f9b301
Prince <justprince9@gmail.com>
2025-08-28 20:16:16 +0400

    feat(styles): added color and font constants

---
+++ constants/colors.constants.ts
@@ -0,0 +1,8 @@
+export const COLORS = {
+  light: {
+    primary: '#5C2684',
+    secondary: '#F5BE0C',
+    tertiary: '#FFFFFF',
+  },
+  dark: {},
+}
---
+++ constants/fonts.constants.ts
@@ -0,0 +1,8 @@
+export const FONTS = {
+  inter: {
+    bold: 'InterBold',
+    light: 'InterLight',
+    regular: 'InterRegular',
+    semibold: 'InterSemiBold',
+  }
+}
