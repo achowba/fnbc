4ef5ae3afa6b93607da219df05ca048cb8d1a67a
Prince <justprince9@gmail.com>
2025-08-31 12:15:10 +0400

    feat(home): added home tab screen

--- app/(drawer)/(tabs)/index.tsx
+++ app/(drawer)/(tabs)/index.tsx
@@ -1,17 +1,36 @@
-import { Link } from 'expo-router';
+import Cards from '@/components/home/Cards';
+import QuickAccess from '@/components/home/QuickAccess';
+import Transactions from '@/components/transactions/Transactions';
+import SearchInput from '@/components/ui/SearchInput';
+import { QUICK_ACTIONS } from '@/data/actions.data';
+import { CARDS } from '@/data/card.data';
+import { TRANSACTIONS } from '@/data/transactions.data';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, View } from 'react-native';
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
-      <Link href="/(login)">Login</Link>
-      <Link href="/transfer-money">Transfer Money</Link>
-    </View>
+    <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
+      <Cards cards={CARDS} />
+      <View style={styles.contentContainer}>
+        <SearchInput />
+        <QuickAccess actions={QUICK_ACTIONS} />
+        <Transactions transactions={TRANSACTIONS} showSectionTitle />
+      </View>
+    </ScrollView>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#FFFFFF',
+    paddingTop: 30,
+    width: '100%',
+  },
+  contentContainer: {
+    padding: 25,
+    width: '100%',
+  },
+});
 
 export default Home;
